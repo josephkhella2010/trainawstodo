3de2761b334ebaf29485e53bdcb052c8fd93342e
Joseph Khella <josephkhella2010@gmail.com>
2025-07-14 10:42:30 +0200

    Fix build path for Amplify

--- frontend/todo/vite.config.ts
+++ frontend/todo/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build", // 👈 tells Vite to output to ./build
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
+});
